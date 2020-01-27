09f6bdd56e4bd3814bc29a6536ffd16d6406cea5
한재용 <yesjyhan@gmail.com>
2020-01-28 06:51:56 +0900

    202001280651

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const path = require("path");
 const url = require("url");
 const isDev = require("electron-is-dev");
 
+const { autoUpdater } = require("electron-updater");
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = 'info';
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
