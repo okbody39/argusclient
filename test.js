d1d3da708544d16552372fe5b5e1f717902897a3
Jay Yong Han <yesjyhan@gmail.com>
2020-12-16 13:01:41 +0900

    202012152300

--- test.js
+++ test.js
@@ -1,3 +1,52 @@
-let ll = '{"test": "111"}';
+const axios = require('axios');
+const https = require('https');
+let __COOKIE__ = "";
 
-console.log(JSON.parse(ll).test);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+let sendXml = ``;
+let viewServer = "172.16.1.73"
+
+
+let RequestUrl = (reqUrl, method, sendXml) => {
+    axios[method](reqUrl, sendXml, {
+        withCredentials: true,
+        headers: {
+            'Content-Type': 'text/xml;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*',
+            'Cookie': __COOKIE__,
+        },
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        }),
+    })
+    .then((response) => {
+        let headers = response.headers['set-cookie'];
+        let _cookie = (headers || []).join(";");
+
+        console.log(reqUrl); //response.data);
+
+        if(_cookie) {
+            console.log("_cookie: ", _cookie);
+            __COOKIE__ = _cookie;
+        }
+        
+
+        if(JSON.stringify(response.headers).indexOf("CID") !== -1) {
+            console.log(response.headers);
+        }
+
+        if(JSON.stringify(response.data).indexOf("CID") !== -1) {
+            console.log(response.data);
+        }
+    });
+};
+
+
+RequestUrl (`https://${viewServer}/portal/webclient/index.html`, 'get', ``);
+
+setTimeout(() => RequestUrl (`https://${viewServer}/portal/locale/ko.json?_=${(new Date()).valueOf()}`, 'get', ''), 2000);
+
+setTimeout(() => RequestUrl (`https://${viewServer}/portal/info.jsp?_=${(new Date()).valueOf()}`, 'get', ''), 4000);
+
+setTimeout(() => RequestUrl (`https://${viewServer}/portal/locale/ko.json?_=${(new Date()).valueOf()}`, 'get', ''), 6000);
